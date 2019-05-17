7758cec45e0cfcd115d8b7c2dbbe743a675675a5
Serhii Radkevych <radserhii@gmail.com>
2019-05-17 23:40:05 +0300

    init node crm

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const keys = require('./config/keys');
+const authRoutes = require('./routes/auth');
+const analyticsRoutes = require('./routes/analytics');
+const categoryRoutes = require('./routes/category');
+const orderRoutes = require('./routes/order');
+const positionRoutes = require('./routes/position');
+
+const app = express();
+
+mongoose.connect(keys.mongoURI)
+    .then(() => console.log('MongoDB Connected'))
+    .catch(error => console.log(error));
+
+app.use(require('morgan')('dev'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(require('cors')());
+
+// Routes
+app.use('/api/auth', authRoutes);
+app.use('/api/analytics', analyticsRoutes);
+app.use('/api/category', categoryRoutes);
+app.use('/api/order', orderRoutes);
+app.use('/api/position', positionRoutes);
+
+module.exports = app;
---
+++ controllers/auth.js
@@ -0,0 +1,9 @@
+const User = require('../models/User');
+
+module.exports.login = (req, res) => {
+
+};
+
+module.exports.register = (req, res) => {
+
+};
